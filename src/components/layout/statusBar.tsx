f0ab121c93452bcdb5bb61a4ba0d7476223a8edc
Arshpreet62 <arshpreet6462@gmail.com>
2025-11-16 01:20:51 +0530

    all usless imports removed

--- src/components/layout/statusBar.tsx
+++ src/components/layout/statusBar.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Story from "../ui/Story";
 
 export default function statusBar({ story }: { story: string }) {
--- src/components/ui/Story.tsx
+++ src/components/ui/Story.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Story({ story }: { story: string }) {
   return (
     <div className="h-full w-full object-fill ">
